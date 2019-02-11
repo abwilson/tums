872ccbde9cfe74b6745af88151878f8d71032031
Ab Wilson <norman.wilson@gmail.com>
2019-02-11 18:24:56 +0000

    Add libdirs to execs

--- features/test.mk
+++ features/test.mk
@@ -1,12 +1,11 @@
 define test_impl
-    $(info Defining test "$1".)    
+    $(call dbg,Defining test "$1".)
+    $(call dbg_obj,$1)
     $(eval test: $(1:%=$(bld)/%.ok))
     $(eval $1.src ?= $(wildcard $(src)/*Test.cpp))
     $(eval $1.libs += gtest_main gtest gmock)
     $(exec_impl)
     $(eval tests += $(target))
-    $(info src in test $(src))
-    $(info bld in test $(bld))
 endef
 
 test = $(eval $(test_impl))
